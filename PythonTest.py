782e348b4387dbec797c3fe6b33e32718ee74ead
TyreeBlakeBailey <84332661+TyreeBlakeBailey@users.noreply.github.com>
2021-05-17 15:56:34 +0100

    Add files via upload

---
+++ PythonTest.py
@@ -0,0 +1,11 @@
+#Get users name,
+# age
+# Date of birth
+
+
+first_name = input("Please enter you first name  ")
+last_name = input("Please enter your last name  ")
+Date_Birth = input("Please enter date of birth  ")
+
+print(first_name + " " + last_name + " " + Date_Birth)
+
